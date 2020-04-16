d6e5179a158479dcc2562b3c31209070f881506c
Erik Weir <erik.weir@key.me>
2020-04-16 18:32:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
- board =  []
- index = []
-end
-def position_taken?(board, index)
-  if   [" ", " ", " ", " ", " ", " ", " ", " ", " "] == [0]
-    return "false"
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
   end
+end
