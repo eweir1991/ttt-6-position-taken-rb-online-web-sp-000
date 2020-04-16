53a688476848494bb69c7d400957828bdeda2189
Erik Weir <erik.weir@key.me>
2020-04-16 18:21:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,6 @@
 # code your #position_taken? method here!
-require_relative '../lib/position_taken.rb'
+
 def position_taken?(board, index)
- Board = [
+ Board =  [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+ index = [0]
 end
